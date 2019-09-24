d3f1312b31aeebe547d02bb6251e66c7b0eba328
Gonzalo <gonzaloaf7@gmail.com>
2019-09-24 14:47:00 -0400

    Este es typescript

---
+++ variables.ts
@@ -0,0 +1,8 @@
+let disabled:boolean = true;//true or false
+console.log(disabled);
+let firtsName:string = "angel";
+console.log(firtsName);
+let greeting:string = `hello ${firtsName}`;
+console.log(greeting);
+let list:Array<number> = [3,7];
+console.log(list);
